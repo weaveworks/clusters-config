7d0b94f6f484b9049ebbc10932a5e6cdf432c2cf
Mohamed Saeed <mohamed.saeed@weave.works>
2022-09-08 15:57:04 +0200

    add admin role to aws-auth after the cluster is created

--- eksctl-clusters/scripts/provision-cluster.sh
+++ eksctl-clusters/scripts/provision-cluster.sh
@@ -10,11 +10,29 @@ then
   exit 1
 fi
 
-CONFIG_FILE=clusters/${CLUSTER_NAME}/eksctl-cluster.yaml
+export AWS_REGION="eu-north-1"
+export WW_ADMIN_ARN="arn:aws:iam::894516026745:role/AdministratorAccess"
+
+export PARENT_DIR=${BASH_SOURCE%/scripts*}
+export CLUSTER_DIR=${PARENT_DIR}/clusters/${CLUSTER_NAME}
+CONFIG_FILE=${CLUSTER_DIR}/eksctl-cluster.yaml
+
+# Chech if GITHUB_TOKEN is set
+if [ -z $GITHUB_TOKEN ]; then
+  echo "Please export your GITHUB_TOKEN so flux can bootstrap!"
+  exit 1
+else 
 
 # Check if the cluster exists from AWS
-export CLUSTER_EXISTS=$(eksctl get clusters | grep -i $CLUSTER_NAME)
+export CLUSTER_EXISTS=$(eksctl get clusters --region ${AWS_REGION}| grep -i ${CLUSTER_NAME})
 if [ -z $CLUSTER_EXISTS ]; then
   # Create EKS cluster
   eksctl create cluster -f ${CONFIG_FILE}
+else
+  echo "There is a cluster created with the same name"
+  exit 1
 fi
+
+# Add WW roles to aws-auth
+echo "Add ${WW_ADMIN_ARN} to aws-auth"
+eksctl create iamidentitymapping --cluster ${CLUSTER_NAME} --region ${AWS_REGION} --arn ${WW_ADMIN_ARN} --group system:masters --username admin
