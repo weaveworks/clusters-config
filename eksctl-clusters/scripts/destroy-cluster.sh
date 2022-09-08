14961c49b59adfb4194fc47f523d1ebbceb61d0b
Mohamed Saeed <mohamed.saeed@weave.works>
2022-09-08 18:02:14 +0200

    add destroy-cluster.sh

--- eksctl-clusters/scripts/destroy-cluster.sh
+++ eksctl-clusters/scripts/destroy-cluster.sh
@@ -1,1 +1,25 @@
 #! /bin/bash
+
+CLUSTER_NAME=$1
+if [ -z $1 ]
+then
+  echo "You have to enter the cluster name."
+  exit 1
+fi
+
+export AWS_REGION="eu-north-1"
+
+export PARENT_DIR=${BASH_SOURCE%/scripts*}
+export CLUSTER_DIR=${PARENT_DIR}/clusters/${CLUSTER_NAME}
+CONFIG_FILE=${CLUSTER_DIR}/eksctl-cluster.yaml
+
+
+export CLUSTER_EXISTS=$(eksctl get clusters --region ${AWS_REGION} | grep -iw ${CLUSTER_NAME})
+if [ -z $CLUSTER_EXISTS ]; then
+  echo "There is no cluster with the provided name"
+  exit 1
+else
+  # Delete EKS cluster
+  echo "Deleting ${CLUSTER_NAME} cluster"
+  eksctl delete cluster -f ${CONFIG_FILE}
+fi
