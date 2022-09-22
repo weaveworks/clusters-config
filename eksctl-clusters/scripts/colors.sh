f0019ff6f072ba982f8a5cfbe1b46b4db17db894
Mohamed Saeed <mohamed.saeed@weave.works>
2022-09-22 08:55:43 +0200

    add colors to log messages

---
+++ eksctl-clusters/scripts/colors.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+
+NC='\033[0m' # No Color
+
+ERROR="${RED}[ERROR]${NC}"
+WARNING="${YELLOW}[WARNING]${NC}"
+SUCCESS="${GREEN}[SUCCESS]${NC}"
