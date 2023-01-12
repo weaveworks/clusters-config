67ec3c2786a3cc888ce7cd957875499bc7ee04df
Eneko Fernandez <eneko@weave.works>
2023-01-12 16:32:19 +0000

    adding tf-module

--- eksctl-clusters/terraform/external-secrets/main.tf
+++ eksctl-clusters/terraform/external-secrets/main.tf
@@ -1,4 +1,37 @@
-resource "helm_release" "external-secrets" {
-  name       = "external-secrets"
-  chart      = "./charts/external-secrets"
+resource "kubectl_manifest" "external_secrets_repo" {
+  yaml_body = <<-YAML
+    apiVersion: source.toolkit.fluxcd.io/v1beta1
+    kind: HelmRepository
+    metadata:
+      name: external-secrets
+      namespace: flux-system
+    spec:
+      interval: 10m
+      url: https://charts.external-secrets.io
+  YAML
+}
+
+resource "kubectl_manifest" "external_secrets_release" {
+  yaml_body = <<-YAML
+    apiVersion: helm.toolkit.fluxcd.io/v2beta1
+    kind: HelmRelease
+    metadata:
+      name: external-secrets
+      namespace: flux-system
+    spec:
+      releaseName: external-secrets
+      targetNamespace: external-secrets
+      interval: 10m
+      chart:
+        spec:
+          chart: external-secrets
+          sourceRef:
+            kind: HelmRepository
+            name: external-secrets
+            namespace: flux-system
+      values:
+        installCRDs: true
+      install:
+        createNamespace: true
+  YAML
 }
