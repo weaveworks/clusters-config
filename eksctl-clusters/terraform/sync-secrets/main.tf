0cd0f1f947fb3633fb469b4b3023f23d1dff07c2
Eneko Fernandez <eneko@weave.works>
2023-01-15 09:11:19 +0000

    sync secrets added

--- eksctl-clusters/terraform/sync-secrets/main.tf
+++ eksctl-clusters/terraform/sync-secrets/main.tf
@@ -21,6 +21,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
 }
 
 
