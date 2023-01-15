0d3e3c04940ad1768f13e9c1a9869b6c4de3c94d
Eneko Fernandez <eneko@weave.works>
2023-01-15 18:32:59 +0000

    external secrets

--- eksctl-clusters/terraform/external-secrets/main.tf
+++ eksctl-clusters/terraform/external-secrets/main.tf
@@ -1,28 +1,32 @@
-provider "aws" {
-  region = "eu-north-1"
+#provider "aws" {
+#  region = "eu-north-1"
+#
+#}
+#data "aws_eks_cluster" "this" {
+#  name = "wge2205"
+#}
+#
+#data "aws_eks_cluster_auth" "this" {
+#  name = "wge2205"
+#}
+#
+#provider "kubectl" {
+#  host                   = data.aws_eks_cluster.this.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#  token                  = data.aws_eks_cluster_auth.this.token
+#  load_config_file       = false
+#}
+#
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.this.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#  token                  = data.aws_eks_cluster_auth.this.token
+#}
 
-}
-data "aws_eks_cluster" "this" {
-  name = "wge2205"
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = "wge2205"
-}
 
 provider "kubectl" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
+  load_config_file = "false"
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
 
 
 resource "kubectl_manifest" "external_secrets_repo" {
