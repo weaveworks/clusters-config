1aeb18a90c786022702ab21593b75099e55dc00b
Eneko Fernandez <eneko@weave.works>
2023-01-15 09:05:37 +0000

    using cluster details

--- eksctl-clusters/terraform/sync-secrets/main.tf
+++ eksctl-clusters/terraform/sync-secrets/main.tf
@@ -1,7 +1,26 @@
+provider "aws" {
+  region = "eu-north-1"
+
+}
+data "aws_eks_cluster" "this" {
+  name = "wge2205"
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = "wge2205"
+}
+
 provider "kubectl" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
 }
 
 provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 
