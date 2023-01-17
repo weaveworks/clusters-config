019041a24d4b0d9daf3c2690870bf14a5f8b1b89
Eneko Fernandez <eneko@weave.works>
2023-01-17 09:31:52 +0000

    adding resources from other cluster

---
+++ eksctl-clusters/terraform/external-secrets/main.tf
@@ -0,0 +1,68 @@
+#provider "aws" {
+#  region = "eu-north-1"
+#
+#}
+#data "aws_eks_cluster" "this" {
+#  name = "wge2205"
+#}
+#
+#data "aws_eks_cluster_auth" "this" {
+#  name = "wge2205"
+#}
+#
+#provider "kubectl" {
+#  host                   = data.aws_eks_cluster.this.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#  token                  = data.aws_eks_cluster_auth.this.token
+#  load_config_file       = false
+#}
+#
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.this.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#  token                  = data.aws_eks_cluster_auth.this.token
+#}
+
+
+provider "kubectl" {
+  load_config_file = "false"
+}
+
+
+resource "kubectl_manifest" "external_secrets_repo" {
+  yaml_body = <<-YAML
+    apiVersion: source.toolkit.fluxcd.io/v1beta1
+    kind: HelmRepository
+    metadata:
+      name: external-secrets
+      namespace: flux-system
+    spec:
+      interval: 10m
+      url: https://charts.external-secrets.io
+  YAML
+}
+
+resource "kubectl_manifest" "external_secrets_release" {
+  yaml_body = <<-YAML
+    apiVersion: helm.toolkit.fluxcd.io/v2beta1
+    kind: HelmRelease
+    metadata:
+      name: external-secrets
+      namespace: flux-system
+    spec:
+      releaseName: external-secrets
+      targetNamespace: external-secrets
+      interval: 10m
+      chart:
+        spec:
+          chart: external-secrets
+          sourceRef:
+            kind: HelmRepository
+            name: external-secrets
+            namespace: flux-system
+      values:
+        installCRDs: true
+      install:
+        createNamespace: true
+  YAML
+}
---
+++ eksctl-clusters/terraform/sync-secrets-remote/main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "eu-north-1"
+
+}
+data "aws_eks_cluster" "this" {
+  name = "wge2205"
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = "wge2205"
+}
+
+data "local_file" "token" {
+  filename = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token = data.local_file.token.content
+  load_config_file       = false
+}
+
+
+resource "kubectl_manifest" "source_namespace" {
+  yaml_body = <<-YAML
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: source-remote
+  YAML
+}
+
+
+resource "kubectl_manifest" "target_namespace" {
+  yaml_body = <<-YAML
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: target-remote
+  YAML
+}
+
+
+data "kubectl_filename_list" "manifests" {
+  pattern = "./secrets/*.yaml"
+}
+
+resource "kubectl_manifest" "secrets" {
+  count     = length(data.kubectl_filename_list.manifests.matches)
+  yaml_body = file(element(data.kubectl_filename_list.manifests.matches, count.index))
+}
+
+
+resource "null_resource" "sync_secret" {
+  depends_on = [kubectl_manifest.source_namespace, kubectl_manifest.target_namespace,kubectl_manifest.secrets]
+  provisioner "local-exec" {
+    command = <<-EOF
+      kubectl get secret -n source-remote -l sync="enabled" -o yaml | sed 's/namespace: .*/namespace: target-remote/' | kubectl apply -f -
+    EOF
+  }
+}
