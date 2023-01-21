b2f4072e0535d070f3e3f5729f3fb9dcd0eedd1c
Waleed Hammam <waleed.hamam@weave.works>
2023-01-22 00:28:15 +0200

    add role

--- eksctl-clusters/leaf-terraform/modules/tf-controller/main.tf
+++ eksctl-clusters/leaf-terraform/modules/tf-controller/main.tf
@@ -6,10 +6,6 @@ data "aws_eks_cluster" "default_test-control-plane" {
   name = "default_test-control-plane"
 }
 
-data "aws_eks_cluster_auth" "default_test-control-plane" {
-  name = "default_test-control-plane"
-}
-
 data "aws_iam_policy_document" "tf_controller_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -33,9 +29,9 @@ resource "aws_iam_role" "tf_controller" {
   name               = "default_test-control-plane-tf-controller"
 }
 
-# resource "aws_iam_role_policy_attachment" "tf_controller" {
-#   role       = aws_iam_role.tf_controller.name
-#   policy_arn = "arn:aws:iam::aws:policy/waleed-tf-runner"
-# }
+resource "aws_iam_role_policy_attachment" "tf_controller" {
+  role       = aws_iam_role.tf_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
 
 # kubectl annotate -n flux-system serviceaccount tf-runner eks.amazonaws.com/role-arn="arn:aws:iam::894516026745:role/wge2205-tf-controller"
