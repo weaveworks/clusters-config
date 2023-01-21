acd73a8583eec76b6869be82ca5e4d295179ab10
Waleed Hammam <waleed.hamam@weave.works>
2023-01-22 00:14:19 +0200

    change role

--- eksctl-clusters/leaf-terraform/main.tf
+++ eksctl-clusters/leaf-terraform/main.tf
@@ -2,13 +2,13 @@ module "tf-controller" {
   source = "./modules/tf-controller"
 }
 
-module "sync-secrets" {
-  source = "./modules/sync-secrets"
-}
+# module "sync-secrets" {
+#   source = "./modules/sync-secrets"
+# }
 
-module "flux" {
-  source = "./modules/flux"
-}
+# module "flux" {
+#   source = "./modules/flux"
+# }
 
 # module "external-secrets" {
 #   source = "./modules/external-secrets"
