7fda00136b2a176e51e35ce14f31141229ecac66
Waleed Hammam <waleed.hamam@weave.works>
2023-01-21 23:04:15 +0200

    move

--- eksctl-clusters/leaf-terraform/main.tf
+++ eksctl-clusters/leaf-terraform/main.tf
@@ -6,18 +6,15 @@ module "tf-controller" {
 module "sync-secrets" {
   source = "./modules/sync-secrets"
   cluster_name = var.CLUSTER_NAME
-  depends_on = [module.tf-controller]
 }
 
 module "flux" {
   source = "./modules/flux"
   cluster_name = var.CLUSTER_NAME
-  depends_on = [module.sync-secrets]
 }
 
 module "external-secrets" {
   source = "./modules/external-secrets"
   cluster_name = var.CLUSTER_NAME
-  depends_on = [module.flux]
 }
 
