e9819a6fad071c8fdb3f47a05cda80118c605b3d
Waleed Hammam <waleed.hamam@weave.works>
2023-01-25 15:41:58 +0200

    sync secrets working

--- eksctl-clusters/leaf-terraform/modules/sync-secrets/main.tf
+++ eksctl-clusters/leaf-terraform/modules/sync-secrets/main.tf
@@ -10,8 +10,12 @@ data "aws_eks_cluster_auth" "this" {
   name = "waleed-tf"
 }
 
-data "local_file" "token" {
-  filename = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+data "aws_eks_cluster" "leaf" {
+  name = "default_leaf-control-plane"
+}
+
+data "aws_eks_cluster_auth" "leaf" {
+  name = "default_leaf-control-plane"
 }
 
 provider "kubectl" {
@@ -19,44 +23,86 @@ provider "kubectl" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token = data.aws_eks_cluster_auth.this.token
   load_config_file       = false
+  alias                  = "this"
 }
 
 
-resource "kubectl_manifest" "source_namespace" {
-  yaml_body = <<-YAML
-    apiVersion: v1
-    kind: Namespace
-    metadata:
-      name: source-remote
-  YAML
+provider "kubectl" {
+ host                   = data.aws_eks_cluster.leaf.endpoint
+ cluster_ca_certificate = base64decode(data.aws_eks_cluster.leaf.certificate_authority[0].data)
+ token                  = data.aws_eks_cluster_auth.leaf.token
+ load_config_file       = false
+ alias                  = "leaf"
 }
 
-
-resource "kubectl_manifest" "target_namespace" {
-  yaml_body = <<-YAML
-    apiVersion: v1
-    kind: Namespace
-    metadata:
-      name: target-remote
-  YAML
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  alias                  = "this"
 }
 
-
-data "kubectl_filename_list" "manifests" {
-  pattern = "./secrets/*.yaml"
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.leaf.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.leaf.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.leaf.token
+  alias                  = "leaf"
 }
 
-resource "kubectl_manifest" "secrets" {
-  count     = length(data.kubectl_filename_list.manifests.matches)
-  yaml_body = file(element(data.kubectl_filename_list.manifests.matches, count.index))
-}
-
-
-resource "null_resource" "sync_secret" {
-  depends_on = [kubectl_manifest.source_namespace, kubectl_manifest.target_namespace,kubectl_manifest.secrets]
-  provisioner "local-exec" {
-    command = <<-EOF
-      kubectl get secret -n source-remote -l sync="enabled" -o yaml | sed 's/namespace: .*/namespace: target-remote/' | kubectl apply -f -
-    EOF
+data "kubernetes_secret_v1" "secret-to-sync-remote" {
+  metadata {
+    name      = "ssh-creds"
+    namespace = "source-remote"
   }
+  provider = kubernetes.this
 }
+
+resource "kubernetes_secret_v1" "target-to-sync-remote" {
+  metadata {
+    name      = data.kubernetes_secret_v1.secret-to-sync-remote.metadata[0].name
+    namespace = data.kubernetes_secret_v1.secret-to-sync-remote.metadata[0].namespace
+  }
+
+  data     = data.kubernetes_secret_v1.secret-to-sync-remote.data
+  provider = kubernetes.leaf
+}
+
+
+# resource "kubectl_manifest" "source_namespace" {
+#   yaml_body = <<-YAML
+#     apiVersion: v1
+#     kind: Namespace
+#     metadata:
+#       name: source-remote
+#   YAML
+# }
+
+
+# resource "kubectl_manifest" "target_namespace" {
+#   yaml_body = <<-YAML
+#     apiVersion: v1
+#     kind: Namespace
+#     metadata:
+#       name: target-remote
+#   YAML
+# }
+
+
+# data "kubectl_filename_list" "manifests" {
+#   pattern = "./secrets/*.yaml"
+# }
+
+# resource "kubectl_manifest" "secrets" {
+#   count     = length(data.kubectl_filename_list.manifests.matches)
+#   yaml_body = file(element(data.kubectl_filename_list.manifests.matches, count.index))
+# }
+
+
+# resource "null_resource" "sync_secret" {
+#   depends_on = [kubectl_manifest.source_namespace, kubectl_manifest.target_namespace,kubectl_manifest.secrets]
+#   provisioner "local-exec" {
+#     command = <<-EOF
+#       kubectl get secret -n source-remote -l sync="enabled" -o yaml | sed 's/namespace: .*/namespace: target-remote/' | kubectl apply -f -
+#     EOF
+#   }
+# }
