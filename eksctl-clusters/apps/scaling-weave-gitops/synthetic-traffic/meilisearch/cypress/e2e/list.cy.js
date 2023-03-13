ce43e36cd034c5fc8285671836f79d971a8519d3
Eneko Fernandez <eneko@weave.works>
2023-03-13 15:19:42 +0000

    using applications index

--- eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/meilisearch/cypress/e2e/list.cy.js
+++ eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/meilisearch/cypress/e2e/list.cy.js
@@ -1,10 +1,10 @@
 const WAITING_TIME = Cypress.env('waitingTime')
 
 describe(`Test search`, () => {
-    it("gets a list of movies", () => {
-        cy.request("GET", "/indexes/movies/search").then((response) => {
+    it("gets a list of applications", () => {
+        cy.request("GET", "/indexes/applications/search").then((response) => {
             expect(response.status).to.eq(200)
-            expect(response.body.hits).to.have.lengthOf(20)
+            // expect(response.body.hits).to.have.lengthOf(20)
         })
     })
 })
