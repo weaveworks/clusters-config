d1669908e49dbeaaaa1a0b39d4f52dafcabc45bb
Eneko Fernandez <eneko@weave.works>
2023-03-23 19:00:54 +0000

    joining together

--- eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/wge-scaling/cypress.config.js
+++
@@ -1,7 +0,0 @@
-const {defineConfig} = require("cypress");
-
-module.exports = defineConfig({
-    e2e: {
-        baseUrl: 'https://wge-2448.eng-sandbox.weave.works',
-    },
-});
--- eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/wge-scaling/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add('login', (username, password) => {
-    cy.session([username, password], () => {
-        cy.visit('/sign_in')
-        cy.get('#email').type(username)
-        cy.get('#password').type(password)
-        cy.get('.MuiButtonBase-root').contains('CONTINUE').click()
-        cy.url().should('contain', '/clusters')
-    })
-})
