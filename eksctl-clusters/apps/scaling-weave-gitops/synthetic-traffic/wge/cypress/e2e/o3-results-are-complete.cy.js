1c24cc5092c44dc5f809f301a89dec90bd5c14db
Eneko Fernandez <eneko@weave.works>
2023-03-26 23:33:41 +0100

    can compute helm releases and kustomizations

--- eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/wge/cypress/e2e/o3-results-are-complete.cy.js
+++ eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/wge/cypress/e2e/o3-results-are-complete.cy.js
@@ -2,7 +2,7 @@ describe('objective3: results are complete and consistent', () => {
     beforeEach(() => {
         cy.login(Cypress.env('WEGO_USERNAME'), Cypress.env('WEGO_PASSWORD'))
     })
-    it('should see consistent result set', () => {
+    it('should see consistent result on helm releases', () => {
         let expectedNumItems = 0
         let currentNumItems = 0
         //Given a set of resources in management cluster
@@ -21,27 +21,81 @@ describe('objective3: results are complete and consistent', () => {
             cy.log("found items:" + expectedNumItems)
         })
 
+        const checkAndReload = (offset, limit) => {
+            cy.request({
+                method: 'POST',
+                url: "/v1/query",
+                failOnStatusCode: true,
+                'body': {
+                    "query": [{"key": "kind", "value": "HelmRelease", "operand": "equal"}, {
+                        "key": "cluster",
+                        "value": "flux-system/leaf-cluster-1",
+                        "operand": "equal"
+                    }], "limit": limit, "offset": offset
+                }
+            }).as('explorer')
+            //then i have the complete data set
+            cy.get('@explorer').then((response) => {
+                let objects = response.body.objects
+                cy.log(objects)
+                cy.log("found items:" + currentNumItems)
+
+                if (objects.length === 0) {
+                    expect(currentNumItems).to.eq(expectedNumItems)
+                    return
+                }
+                currentNumItems += objects.length
+                checkAndReload(offset + limit, limit)
+            })
+        }
+        checkAndReload(0, 25)
+    })
+    it('should see consistent result on kustomization', () => {
+        let expectedNumItems = 0
+        let currentNumItems = 0
+        //Given a set of resources in management cluster
         cy.request({
-            method: 'POST',
-            url: "/v1/query",
-            failOnStatusCode: true,
-            'body': {
-                "query": [{"key": "kind", "value": "HelmRelease", "operand": "equal"}, {
-                    "key": "cluster",
-                    "value": "flux-system/leaf-cluster-1",
-                    "operand": "equal"
-                }], "limit": 25, "offset": 25
-            }
-        }).as('explorer')
-        //then i have the complete data set
-        cy.get('@explorer').then((response) => {
-            let objects = response.body.objects
-            cy.log(objects)
-            currentNumItems = objects.length
-            cy.log("found items:" + currentNumItems)
+            method: 'GET',
+            url: Cypress.env('LEAF_URL') + "/apis/kustomize.toolkit.fluxcd.io/v1beta2/kustomizations?limit=500",
+            failOnStatusCode: false,
+            'auth': {
+                'bearer': Cypress.env('LEAF_TOKEN')
+            },
+        }).as('details')
+        cy.get('@details').its('status').should('eq', 200)
+        cy.get('@details').then((response) => {
+            let body = response.body
+            expectedNumItems = body.items.length
+            cy.log("found items:" + expectedNumItems)
         })
 
-        cy.should(currentNumItems == expectedNumItems)
+        const checkAndReload = (offset, limit) => {
+            cy.request({
+                method: 'POST',
+                url: "/v1/query",
+                failOnStatusCode: true,
+                'body': {
+                    "query": [{"key": "kind", "value": "Kustomization", "operand": "equal"}, {
+                        "key": "cluster",
+                        "value": "flux-system/leaf-cluster-1",
+                        "operand": "equal"
+                    }], "limit": limit, "offset": offset
+                }
+            }).as('explorer')
+            //then i have the complete data set
+            cy.get('@explorer').then((response) => {
+                let objects = response.body.objects
+                cy.log(objects)
+                cy.log("found items:" + currentNumItems)
 
+                if (objects.length === 0) {
+                    expect(currentNumItems).to.eq(expectedNumItems)
+                    return
+                }
+                currentNumItems += objects.length
+                checkAndReload(offset + limit, limit)
+            })
+        }
+        checkAndReload(0, 25)
     })
 })
