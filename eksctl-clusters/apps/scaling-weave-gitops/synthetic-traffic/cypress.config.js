cd0a24ed1446b154805af1ea9150f294f38ee883
Eneko Fernandez <eneko@weave.works>
2023-03-27 10:14:37 +0100

    creating new structure based on objectives

--- eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/cypress.config.js
+++ eksctl-clusters/apps/scaling-weave-gitops/synthetic-traffic/cypress.config.js
@@ -1,10 +1,6 @@
 const {defineConfig} = require("cypress");
-const {cloudPlugin} = require("cypress-cloud/plugin");
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'https://wge-2448.eng-sandbox.weave.works',
-        setupNodeEvents(on, config) {
-            return cloudPlugin(on, config);
-        },
     },
 });
